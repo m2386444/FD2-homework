4b8334027dfb0fd218292410ca714ecbd74658b9
PavelMasharov <2386444@gmail.com>
2018-07-27 15:03:20 +0300

    n33

--- n33_clockDom.js
+++ n33_clockDom.js
@@ -1,17 +1,15 @@
 'use strict'
 const HOURS = 12; //digits on the dial
 const ANGLE_TO_NEXT_DIGIT = 30;
-
 var clockBoard = document.getElementById('clock');
-
 
 (function (){
     //crate and set position to center of clock board
         var clockBoardCenter = document.createElement('div');
     clockBoardCenter.className += 'clockCenter';
     clockBoardCenter.style.position = 'absolute';
-    clockBoardCenter.style.left = '150.5px';
-    clockBoardCenter.style.top = '150.5px';
+    clockBoardCenter.style.left = clockBoard.offsetWidth/2 + 'px';
+    clockBoardCenter.style.top = clockBoard.offsetHeight/2 + 'px';
     clockBoard.appendChild(clockBoardCenter);
     
     //crate and set position to hour hands
@@ -30,21 +28,18 @@ var clockBoard = document.getElementById('clock');
     secondLine.disabled = true;
     secondLine.className += 'lin secondLin secondTransform';
     clockBoardCenter.appendChild(secondLine);
-    
-    
-    
+
     //create and set position to digits of the dial
-    var radius = 120;
-    var _angle = 30;
-    
+    var radius = clockBoard.offsetHeight/2.5;
+    var _angle = 360/12;
     for (var i=1; i<=HOURS; i++) {
         var curNum = document.createElement('input');
         curNum.type = 'button'
         curNum.disabled = true;
         curNum.value = [i];
         curNum.className += 'num';
-        curNum.style.width = '40px';
-        curNum.style.height = '40px';
+        curNum.style.width = clockBoard.offsetWidth/7 + 'px';
+        curNum.style.height = clockBoard.offsetHeight/7 + 'px';
         var angle = _angle/180*Math.PI;
         var centCenterX = clockBoardCenter.offsetLeft+clockBoardCenter.offsetWidth/2;
         var centCenterY = clockBoardCenter.offsetTop+clockBoardCenter.offsetHeight/2;
@@ -59,17 +54,9 @@ var clockBoard = document.getElementById('clock');
     var dial = document.createElement('span');
     dial.id = 'di';
     clockBoard.appendChild(dial);
-    
+    updateTime();
     //update time functions
-    var timer = 0;
-    timer = setTimeout(updateTimeStopTimeout, 0);
     setInterval(updateTime, 1000);
-    function updateTimeStopTimeout () {
-        updateTime();
-        if (timer !== 0) {
-            clearTimeout(timer);
-        }
-    }
     function updateTime() {
         var curTime = new Date();
         var curTimeStr = formatDateTime(curTime);
@@ -79,86 +66,22 @@ var clockBoard = document.getElementById('clock');
         var hours=dt.getHours();
         var minutes=dt.getMinutes();
         var seconds=dt.getSeconds();
-        
-        function setPositions() {
-            var hou = 0;
-            hou = 0;
-            var minAngle = 0;
-            var houAngle = 0;
-            
-            function getHou (param) {
-                switch(param) {
-                    case 24:
-                    hou = 12
-                    break;
-                    case 23:
-                    hou = 11
-                    break;
-                    case 22:
-                    hou = 10
-                    break;
-                    case 21:
-                    hou = 9
-                    break;
-                    case 20:
-                    hou = 8
-                    break;
-                    case 19:
-                    hou = 7
-                    break;
-                    case 18:
-                    hou = 6
-                    break;
-                    case 17:
-                    hou = 5
-                    break;
-                    case 16:
-                    hou = 4
-                    break;
-                    case 15:
-                    hou = 3
-                    break;
-                    case 14:
-                    hou = 2
-                    break;
-                    case 13:
-                    hou = 1
-                    break;
-                    default:
-                    hou = param;
-                }
-            }
-            getHou(hours);
-            
-            function getMinHouAngle(paramSec, paramMin) {
-                if (paramSec == 0) {
-                    minAngle = minutes/60*360;
-                } else {
-                    minAngle = minutes/60*360+360/60*seconds/60;
-                }
-                if (paramMin == 0) {
-                    houAngle = hou/12*360;
-                } else {
-                    houAngle = hou/12*360+360/12*minutes/60;
-                }
-            }
-            getMinHouAngle(seconds, minutes);
-            
-            function secondGo() {
-                secondLine.style.transform = 'translateY(-130px) translateX(-0.25px) rotate('+seconds/60*360+'deg)';
-                minuteLine.style.transform =  'translateY(-105px) translateX(-1.25px) rotate('+ minAngle +'deg';
-                hourLine.style.transform = 'translateY(-50px) translateX(-2px) rotate('+ houAngle +'deg)';
-            }
-            secondGo(seconds);
-        }
-        setPositions();
         function str0l(val,len) {
             var strVal=val.toString();
             while ( strVal.length < len )
             strVal='0'+strVal;
             return strVal;
         }
-        clockBoard.style.opacity = '1';
+        function setPositions() {
+            var minAngle = 0;
+            var houAngle = 0;
+            minAngle = minutes/60*360+360/60*seconds/60;
+            houAngle = hours%12/12*360+360/12*minutes/60;
+            secondLine.style.transform = 'translateY(' + (-secondLine.offsetHeight) + 'px)' + 'translateX(' + (-secondLine.offsetWidth/2) + 'px)' + 'rotate(' + seconds/60*360 + 'deg)';
+            minuteLine.style.transform = 'translateY(' + (-minuteLine.offsetHeight) + 'px)' + 'translateX(' + (-minuteLine.offsetWidth/2) + 'px)' + 'rotate(' + minAngle + 'deg';
+            hourLine.style.transform = 'translateY(' + (-hourLine.offsetHeight) + 'px)' + 'translateX(' + (-hourLine.offsetWidth/2) + 'px)' + 'rotate(' + houAngle + 'deg)';
+        }
+        setPositions();
         return str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2);
     }
 })();
